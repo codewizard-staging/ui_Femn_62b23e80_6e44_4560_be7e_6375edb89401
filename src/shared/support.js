f95208d7ef58b42ccca082d14b79f79e05d59c04
snvkgram <ramgood@gmail.com>
2024-10-04 12:24:43 +0000

    initial commit

---
+++ src/shared/support.js
@@ -0,0 +1,112 @@
+import {
+	
+		
+ 	 	
+		
+ 	
+		
+ 	   SetOrganizationSingle, SetEmployeePersonalInfoSingle, SetEmlpoyeeProfileSingle
+} from "./services";
+import Helper from "shared/helper";
+
+var fn = {};
+
+const defaultError = "Something went wrong while processing request!";
+
+		
+     
+fn.AddOrUpdateOrganization = async (input, enums, excludesItems) => {
+    return new Promise(async (resolve) => {
+        let data = {}, status = false, id = null;
+        let excludes = excludesItems || [];
+        Object.values(input)
+            .filter((x) => x.value)
+            .map((x) => {
+                if (excludes.indexOf(x.key) === -1) {
+                    if (x.type === 'dropdown') {
+                        data[x.key] = enums.find((z) => z.Name === x.source).Values.find((m) => parseInt(m[x.valueId]) === parseInt(x.value))[x.valueId];
+                    } else {
+                        data[x.key] = x.value;
+                    }
+                }
+            });
+
+        global.Busy(true);
+        let rslt = await SetOrganizationSingle(data);
+        global.Busy(false);
+        if (rslt.status) {
+            id = rslt.id;
+            status = true;
+        } else {
+            const msg = rslt.statusText || defaultError;
+            global.AlertPopup("error", msg);
+        }
+
+        return resolve({ status, id });
+    });
+}
+		
+     
+fn.AddOrUpdateEmployeePersonalInfo = async (input, excludesItems) => {
+    return new Promise(async (resolve) => {
+        let data = {}, status = false, id = null;
+        let excludes = excludesItems || [];
+        Object.values(input)
+            .filter((x) => x.value)
+            .map((x) => {
+                if (excludes.indexOf(x.key) === -1) {
+                    data[x.key] = x.value;
+                }
+            });
+
+        global.Busy(true);
+        let rslt = await SetEmployeePersonalInfoSingle(data);
+        global.Busy(false);
+        if (rslt.status) {
+            id = rslt.id;
+            status = true;
+        } else {
+            const msg = rslt.statusText || defaultError;
+            global.AlertPopup("error", msg);
+        }
+
+        return resolve({ status, id });
+    });
+}
+		
+     
+fn.AddOrUpdateEmlpoyeeProfile = async (input, enums, excludesItems) => {
+    return new Promise(async (resolve) => {
+        let data = {}, status = false, id = null;
+        let excludes = excludesItems || [];
+        Object.values(input)
+            .filter((x) => x.value)
+            .map((x) => {
+                if (excludes.indexOf(x.key) === -1) {
+                    if (x.type === 'dropdown') {
+                        data[x.key] = enums.find((z) => z.Name === x.source).Values.find((m) => parseInt(m[x.valueId]) === parseInt(x.value))[x.valueId];
+                    } else {
+                        data[x.key] = x.value;
+                    }
+                }
+            });
+
+        global.Busy(true);
+        let rslt = await SetEmlpoyeeProfileSingle(data);
+        global.Busy(false);
+        if (rslt.status) {
+            id = rslt.id;
+            status = true;
+        } else {
+            const msg = rslt.statusText || defaultError;
+            global.AlertPopup("error", msg);
+        }
+
+        return resolve({ status, id });
+    });
+}
+
+
+
+
+export default fn;
